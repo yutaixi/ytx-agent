58f9bfe39f4c7d4605506c069c725651c613f9e4
ytx <395005958@qq.com>
2024-09-20 23:54:46 +0800

    优化代码

---
+++ src/main/java/com/ytx/ai/agent/controller/AgentController.java
@@ -0,0 +1,24 @@
+package com.ytx.ai.agent.controller;
+
+import com.ytx.ai.agent.MasterAgent;
+import com.ytx.ai.agent.dto.ChatDTO;
+import com.ytx.ai.agent.vo.AgentResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/agent")
+public class AgentController {
+
+    @Autowired
+    MasterAgent masterAgent;
+
+    @PostMapping("/chat")
+    public AgentResponse chat(@RequestBody ChatDTO chatDTO){
+        return masterAgent.run(chatDTO);
+    }
+
+}
